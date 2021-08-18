cf2ab20c377b501471dd7dd5647ca4a85c7090f9
Jonatan Gall <jonatangd.souza@gmail.com>
2021-08-18 00:06:50 -0300

    fixes and implement microservice bank

---
+++ Microservices/Banking/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,21 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Application.Services {
+    public class AccountService : IAccountService {
+        private readonly IAccountRepository _respository;
+
+        public AccountService(IAccountRepository respository) {
+            _respository = respository;
+        }
+        public IEnumerable<Account> GetAccounts() {
+            return _respository.GetAccounts();
+        }
+    }
+}
